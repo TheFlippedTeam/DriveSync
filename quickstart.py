ddf58d40c052620a655a8883636c8449c86617d5
Emanuele <dalorema1006@gmail.com>
2021-02-06 17:28:58 +0100

    fix upload

--- quickstart.py
+++ quickstart.py
@@ -11,13 +11,36 @@ SCOPES = ['https://www.googleapis.com/auth/drive']
 creds = ''
 service = build('drive', 'v3', credentials=creds)
 
+
 def upload(file_metadata, media):
     url = 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media'
-    #file_metadata = {'name': 'photo.jpg'}
-    #media = MediaFileUpload('files/photo.jpg', mimetype='image/jpeg')
-    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
 
-    print("file ID is " + file.get(id))
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds)
+
+    file = service.files().create(body=file_metadata,
+                                  media_body=media, fields='id').execute()
+
+    print("file ID is ", file['id'])
+
 
 def main():
     """Shows basic usage of the Drive v3 API.
@@ -43,9 +66,8 @@ def main():
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
-    #service = build('drive', 'v3', credentials=creds)
+    service = build('drive', 'v3', credentials=creds)
 
-    '''
     # Call the Drive v3 API
     results = service.files().list(
         pageSize=10, fields="nextPageToken, files(id, name)").execute()
@@ -57,11 +79,11 @@ def main():
         print('Files:')
         for item in items:
             print(u'{0} ({1})'.format(item['name'], item['id']))
-    '''
 
-    
+
 if __name__ == '__main__':
-    main()
+    # main()
+
     file_metadata = {'name': 'test.pdf'}
     media = MediaFileUpload('test.pdf', mimetype='application/pdf')
     upload(file_metadata, media)
